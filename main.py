ab71a02e86296705117c2b96471fe5c65b425efe
Charles Pousset <charlespousset@hotmail.fr>
2020-04-20 19:53:58 +0200

    ajout d'une page de jeu
    ajout de la page de jeu à partir d'un deck créer lors d'une nouvelle partie + ajout d'un template création du fichier style CSS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,request,redirect,url_for,session
 from intelligence import *
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.secret_key="tmp"
@@ -10,6 +10,7 @@ app.secret_key="tmp"
 #db=SQLAlchemy(app)
 
 global new_game
+games = []
 
 #class Game(db.Model):
 #	id_game=db.Column("id", db.Integer, primary_key=True) 
@@ -29,6 +30,7 @@ def create_game():
 		new_game=Lobby(new_game_id,new_game_nb_players)
 		print(new_game.status())
 		print(type(new_game.number_of_players))
+		games.append(new_game)
 		# session["game"]=new_game
 		return redirect(url_for("add_player"))
 	return render_template("create_game.html")
@@ -45,6 +47,18 @@ def add_player():
 		print(new_game.status())
 		return redirect(url_for("add_player"))
 	return render_template("add_player.html")
+
+@app.route('/<Lobby_id>', methods=["POST","GET"])
+def game(Lobby_id):
+	global new_game
+	new_game.start_game()
+	# deck_test = ["Vol", "Chef", "Rame","Charge","Tambour","Cellule","Sol","Chemise","Solution","Chou","Mousse","Numéro","Marche","Perle","Carte","Couronne","Carrière","Portable","Lunettes","Sortie","Chaine","Botte","Corne","Mineur","Mine"]
+	deck_test = new_game.deck
+	if request.method == "POST":
+		player_guess = request.form["guess"]
+		new_game.guess(player.player_id, player.color, player.role, player_guess)
+
+	return render_template("game.html", le_deck_de_mots = deck_test)
 
 if __name__=="__main__":
 	#db.create.all()

159ebc1feeeee2cf9b7a3fbef4f8854981e8a0f1
Lucas-M-96 <lucasmainguet@hotmail.fr>
2020-04-21 15:15:47 +0200

    Partage pour correction bugs URL et couleurs
    Problème de l'URL du game.html
    Problème des nombres de cartes de chaque couleur

--- main.py
+++ main.py
@@ -12,19 +12,10 @@ app.secret_key="tmp"
 global list_games
 list_games={}
 
-#class Game(db.Model):
-#	id_game=db.Column("id", db.Integer, primary_key=True) 
-#	object_game=db.Column("object_game", Lobby)
-#
-#	def __init__(self,my_id,my_lobby):
-#		self.id_game=my_id
-#		self.object_game=my_lobby
-
 @app.route("/create_game", methods=["POST","GET"])
 def create_game():
 	global list_games
 	if request.method=="POST":
-		# INSERER UN TEST POUR VOIR SI LA PARTIE EXISTE
 		new_game_id=request.form["new_game_id"]
 		session["game_id"]=new_game_id
 		if new_game_id not in list_games:
@@ -64,14 +55,14 @@ def add_player():
 
 @app.route('/<Lobby_id>', methods=["POST","GET"])
 def game(Lobby_id):
-	global new_game
-	new_game.start_game()
+	global list_games
+	list_games[session["game_id"]].start_game()
 	# deck_test = ["Vol", "Chef", "Rame","Charge","Tambour","Cellule","Sol","Chemise","Solution","Chou","Mousse","Numéro","Marche","Perle","Carte","Couronne","Carrière","Portable","Lunettes","Sortie","Chaine","Botte","Corne","Mineur","Mine"]
-	deck_test = new_game.deck
+	deck_test = list_games[session["game_id"]].deck
 	if request.method == "POST":
 		player_guess = request.form["guess"]
-		new_game.guess(player.player_id, player.color, player.role, player_guess)
-	print(new_game.status())
+		list_games[session["game_id"]].guess(player.player_id, player.color, player.role, player_guess)
+	print(list_games[session["game_id"]].status())
 	return render_template("game.html", le_deck_de_mots = deck_test)
 
 if __name__=="__main__":
